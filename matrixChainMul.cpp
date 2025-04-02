5bc4f9cd39b7f28d9d07d2fa9e96cd48d5e8eff5
akashpg-M <waybooster.pg@gmail.com>
2025-04-02 23:03:50 +0530

    Matrix chain multiplication

--- matrixChainMul.cpp
+++ matrixChainMul.cpp
@@ -1,7 +1,27 @@
-r1  * r2 * c1
-r1  *  
+#include<iostream>
+#include<vector>
+using namespace std;
 
-{ 10, 20, 30, 40, 30 }
+int MinMatrixOper(vector<int>& mDim, int s, int e){
+  if(s == e) return 0;
 
-------------------------------------
+  int res = INT_MAX;
+  for(int i=s; i<e; i++){
+    res = min(res, MinMatrixOper(mDim, s,i) + MinMatrixOper(mDim, i+1, e) + mDim[s-1]*mDim[i]*mDim[e]);
+  }
 
+  return res;
+}
+
+int main(){
+  int n;
+  cout<<"Enter no of Matrix: ";
+  cin>>n;
+  vector<int> mDim(n+1);
+  for(int i=1; i<n+1; i++){
+    cout<<"Enter val: ";
+    cin>>mDim[i];
+  }
+
+  cout<<"Minimum operations needed to multiply matrix : "<<MinMatrixOper(mDim, 1, n-1);
+}
