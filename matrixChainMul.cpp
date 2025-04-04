ba57afb6c912cd54277a93f3ff455283282958a7
akashpg-M <waybooster.pg@gmail.com>
2025-04-04 23:40:37 +0530

    compiled and run matrix Chain Multiplication

--- matrixChainMul.cpp
+++ matrixChainMul.cpp
@@ -1,27 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<climits>
+
 using namespace std;
 
-int MinMatrixOper(vector<int>& mDim, int s, int e){
-  if(s == e) return 0;
-
-  int res = INT_MAX;
-  for(int i=s; i<e; i++){
-    res = min(res, MinMatrixOper(mDim, s,i) + MinMatrixOper(mDim, i+1, e) + mDim[s-1]*mDim[i]*mDim[e]);
-  }
-
-  return res;
-}
+int MinMatrixOper(vector<int>& mDim);
 
 int main(){
   int n;
   cout<<"Enter no of Matrix: ";
   cin>>n;
   vector<int> mDim(n+1);
-  for(int i=1; i<n+1; i++){
+  for(int i=0; i<n+1; i++){
     cout<<"Enter val: ";
     cin>>mDim[i];
   }
 
-  cout<<"Minimum operations needed to multiply matrix : "<<MinMatrixOper(mDim, 1, n-1);
+  cout<<"Minimum operations needed to multiply matrix : "<<MinMatrixOper(mDim);
+}
+
+int MinMatrixOper(vector<int>& mDim){
+  int n = mDim.size()-1;
+  vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+  for(int len = 2; len<=n; len++){
+    for(int i=1; i<=n-len+1; i++){
+      int j=i+len-1;
+      dp[i][j] = INT_MAX;
+
+      for(int k=i; k<j; k++){
+        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + mDim[i-1]*mDim[k]*mDim[j]);
+      }
+    }
+  }
+
+  return dp[1][n];
 }
